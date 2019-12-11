7da656eb7810cbc65337b3684d9e810f41376955
Damaso Fernandez <damasofc@hotmail.com>
2019-12-11 12:10:38 -0600

    agregado el timer.h y hasta aca el paralelo se aplica dentro de la funcion applyFilter

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@
 #include <vector>
 #include <Magick++.h>
 #include <omp.h>
+#include "timer.h"
 
 using namespace std;
 
 using namespace Magick;
 using namespace boost::filesystem;
-
+// ./main -s -f ./imagesTest/ ./imagesProccesed/
 
 Image applyFilterGrayScale(Image &image);
 Image applyFilterGrayScaleParallel(Image &image,int numT);
@@ -21,7 +22,7 @@ Image applyFilterGrayScaleParallel(Image &image,int numT);
 vector<vector<double>> getGaussianKernel(int w, int h,double sigma);
 int main( int argc, char const *argv[])
 {
-
+  double start,finish;
   InitializeMagick(*argv);
   if(argc >= 3){
 		string type = argv[1];
@@ -33,7 +34,7 @@ int main( int argc, char const *argv[])
       path p(argv[3]);
       directory_iterator itr(p);
       if(type.compare("-s") == 0){
-
+        GET_TIME(start);
         for (auto i : itr)
         {
           string img = i.path().string();
@@ -41,16 +42,21 @@ int main( int argc, char const *argv[])
           cout<<argv[4]<<img.substr(2)<<endl;
           applyFilterGrayScale(model).write(argv[4]+img.substr(2));
         }
+        GET_TIME(finish);
       }
       else
       {
+        GET_TIME(start);
+        int threads = stoi(argv[argc -1]);
         for (auto i : itr)
         {
           string img = i.path().string();
           model = Image(img);
           cout<<argv[4]<<img.substr(2)<<endl;
-          applyFilterGrayScaleParallel(model,2).write(argv[4]+img.substr(2));
+          applyFilterGrayScaleParallel(model,threads).write(argv[4]+img.substr(2));
         }
+        GET_TIME(finish);
+        cout<<"Num Threads: "<<threads<<endl;
       }
     }
     else
@@ -58,21 +64,31 @@ int main( int argc, char const *argv[])
       if(type.compare("-s") == 0){
         model = Image(argumentTwo);
         if(argc > 3){
+          GET_TIME(start);
           applyFilterGrayScale(model).write(argv[4]);
+          GET_TIME(finish);
         }else
         {
+          GET_TIME(start);
           applyFilterGrayScale(model).write("newGrayScaleImage.jpeg");
+          GET_TIME(finish);
         }
       }
       else
       {
+        int threads = stoi(argv[argc -1]);
         model = Image(argumentTwo);
         if(argc > 3){
+          GET_TIME(start);
           applyFilterGrayScaleParallel(model,2).write(argv[4]);
+          GET_TIME(finish);
         }else
         {
+          GET_TIME(start);
           applyFilterGrayScaleParallel(model,2).write("newGrayScaleImage.jpeg");
+          GET_TIME(finish);
         }
+        cout<<"\nNum Threads: "<<threads<<endl;
       }
        
     }
@@ -81,47 +97,18 @@ int main( int argc, char const *argv[])
   {
     cout<<"You have to run the program like this examples: "<<endl;
     cout<<"\t0. ./main -s <image_path> "<<endl;
-    cout<<"\t1. ./main -p <image_path> "<<endl;
+    cout<<"\t1. ./main -p <image_path> <# de threads> "<<endl;
     cout<<"\t2. ./main -s <image_path> -name <new_image_path> "<<endl;
-    cout<<"\t3. ./main -p <image_path> -name <new_image_path> "<<endl;
+    cout<<"\t3. ./main -p <image_path> -name <new_image_path> <# de threads> "<<endl;
     // cout<<"\t2. ./main -gaus <image_path> -name <new_image_path> "<<endl;
-    cout<<"\t4. ./main -p -f <image_folders> <image_folder_destination> "<<endl;
+    cout<<"\t4. ./main -p -f <image_folders> <image_folder_destination> <# de threads> "<<endl;
     cout<<"\t4. ./main -s -f <image_folders> <image_folder_destination> "<<endl;
     // cout<<"\t4. ./main -gaus -f <image_folders> "<<endl;
     return 0;
   }
 
-}
+  cout<<"\nTiempo de ejecucion: "<<finish-start<<endl;
 
-Image applyFilterGaussian(Image &image, vector<vector<double>> kernel)
-{
-  int imageH = image.rows();
-  int imageW = image.columns();
-  int kernelH = kernel.size();
-  int kernelW = kernel[0].size();
-
-  Image imgN(Geometry(imageW,imageH),"white");
-  imgN.type(ImageType::TrueColorType);
-  imgN.modifyImage();
-
-  MagickCore::Quantum *pixels = imgN.getPixels(0,0,imageW,imageH);
-  int i,j,h,w;
-
-  for (i=0 ; i<imageH ; i++) {
-    for (j=0 ; j<imageW ; j++) {
-      unsigned offset = imgN.channels() * (imageW * i + j);
-      for (h=i ; h<i+kernelH ; h++) {
-        for (w=j ; w<j+kernelW ; w++) {
-          pixels[offset + 0] += kernel[h-i][w-j] * image.pixelColor(w,h).quantumRed();
-          pixels[offset + 1] += kernel[h-i][w-j] * image.pixelColor(w,h).quantumGreen();
-          pixels[offset + 2] += kernel[h-i][w-j] * image.pixelColor(w,h).quantumBlue();
-        }
-      }
-    }
-  }
-
-  imgN.syncPixels();
-  return imgN;
 }
 
 Image applyFilterGrayScale(Image &image)
@@ -163,7 +150,7 @@ Image applyFilterGrayScaleParallel(Image &image,int numT)
 
   MagickCore::Quantum *pixels = imgN.getPixels(0,0,imageW,imageH);
 
-  #pragma omp parallel for num_threads(numT) schedule(dynamic)
+  #pragma omp parallel for num_threads(numT) schedule(static,5)
   for (int i=0 ; i<imageH ; i++) {
     for (int j=0 ; j<imageW ; j++) {
       unsigned offset = imgN.channels() * (imageW * i + j);
